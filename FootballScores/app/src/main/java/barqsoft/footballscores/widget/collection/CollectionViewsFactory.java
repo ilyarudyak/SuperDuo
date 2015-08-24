055c3ec8b9b72a01c02df77272edcc5ec44b3658
Ilya Rudyak <ilyarudyak@yahoo.com>
2015-08-24 21:40:15 +0300

    Add collection widget (1st draft)

--- FootballScores/app/src/main/java/barqsoft/footballscores/widget/collection/CollectionViewsFactory.java
+++ FootballScores/app/src/main/java/barqsoft/footballscores/widget/collection/CollectionViewsFactory.java
@@ -1,9 +1,15 @@
 package barqsoft.footballscores.widget.collection;
 
+import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
+
+import java.util.List;
+
+import barqsoft.footballscores.R;
+import barqsoft.footballscores.api.Match;
 
 /**
  * Created by ilyarudyak on 8/24/15.
@@ -11,52 +17,65 @@ import android.widget.RemoteViewsService;
 public class CollectionViewsFactory implements
         RemoteViewsService.RemoteViewsFactory {
 
-    public CollectionViewsFactory(Context context, Intent intent) {
+    private List<Match> mMatches;
+    private Context mContext;
+    private int mAppWidgetId;
 
+    public CollectionViewsFactory(Context context, Intent intent, List<Match> matches) {
+        mContext = context;
+        mAppWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                AppWidgetManager.INVALID_APPWIDGET_ID);
+        mMatches = matches;
     }
 
+    // build Remote Views
+    @Override
+    public RemoteViews getViewAt(int position) {
+
+        RemoteViews row = new RemoteViews(mContext.getPackageName(), R.layout.small_widget);
+        setRow(row, position);
+
+        return row;
+    }
+
+    // helper methods
+    private void setRow(RemoteViews row, int position) {
+        row.setTextViewText(R.id.home_name, mMatches.get(position).getHome());
+    }
+
+    // simple getters
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+    @Override
+    public int getCount() {
+        return mMatches.size();
+    }
+
+    // empty methods
     @Override
     public void onCreate() {
-
     }
-
     @Override
     public void onDataSetChanged() {
 
     }
-
     @Override
     public void onDestroy() {
 
     }
-
-    @Override
-    public int getCount() {
-        return 0;
-    }
-
-    @Override
-    public RemoteViews getViewAt(int position) {
-        return null;
-    }
-
     @Override
     public RemoteViews getLoadingView() {
         return null;
     }
 
-    @Override
-    public int getViewTypeCount() {
-        return 0;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
-
-    @Override
-    public boolean hasStableIds() {
-        return false;
-    }
 }
--- FootballScores/app/src/main/java/barqsoft/footballscores/widget/collection/CollectionWidgetProvider.java
+++ FootballScores/app/src/main/java/barqsoft/footballscores/widget/collection/CollectionWidgetProvider.java
@@ -1,11 +1,40 @@
 package barqsoft.footballscores.widget.collection;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
+
+import barqsoft.footballscores.R;
 
 /**
  * Created by ilyarudyak on 8/24/15.
  */
 public class CollectionWidgetProvider extends AppWidgetProvider {
 
-    
+    public static final String EXTRA_MATCH = "barqsoft.footballscores.widget.collection.MATCH";
+    public static final String EXTRA_MATCHES = "barqsoft.footballscores.widget.collection.MATCHES";
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+        for (int appWidgetId : appWidgetIds) {
+
+            // create intent to start service
+            Intent serviceIntent = new Intent(context, CollectionWidgetService.class);
+            serviceIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+            serviceIntent.setData(Uri.parse(serviceIntent.toUri(Intent.URI_INTENT_SCHEME)));
+
+            // set remote adapter; widget has only ListView with id == words
+            RemoteViews widget = new RemoteViews(context.getPackageName(), R.layout.collection_widget);
+            widget.setRemoteAdapter(R.id.matches_list_view, serviceIntent);
+
+            // and here we use manager to update remote views
+            appWidgetManager.updateAppWidget(appWidgetId, widget);
+        }
+
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
 }
