02392cbc44fd3ec2c3fb992793b389a5fcdc4634
Ilya Rudyak <ilyarudyak@yahoo.com>
2015-10-30 16:39:36 +0300

    fix bug with screen rotation

--- Alexandria/app/src/main/java/it/jaschke/alexandria/LibraryFragment.java
+++ Alexandria/app/src/main/java/it/jaschke/alexandria/LibraryFragment.java
@@ -85,10 +85,6 @@ public class LibraryFragment extends Fragment
             }
         });
 
-        if (cursor != null) {
-            cursor.close();
-        }
-
         return rootView;
     }
 
